331c0dcd1e2d1cfe1b2213de5291c536a9ed7ef8
el-davo <david.ahrn26@gmail.com>
2018-12-15 21:51:56 +0000

    Go getting

---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import humanize "github.com/dustin/go-humanize"
+import "fmt"
+
+func main() {
+  fmt.Println("hello world")
+  fmt.Printf("That file is %s.\n", humanize.Bytes(82854982)) // That file is 83 MB.
+  fmt.Printf("You're my %s best friend.\n", humanize.Ordinal(193)) // You are my 193rd best friend.
+  fmt.Printf("You owe $%s.\n", humanize.Comma(6582491)) // You owe $6,582,491.
+}
